29c3aac18aff09b5d87337d148723a6341bf22bf
Ricardo Lucas <15297@alunos.ipbeja.pt>
2018-05-17 16:51:08 +0100

    segundo commit

--- questao6.py
+++ questao6.py
@@ -3,29 +3,6 @@ import subprocess
 import string 
 from questao1 import *
 
-resposta_input = raw_input("Pretende eliminar o que?(1-zona forward /n 2-virtualhost /n 3-zona reverse):")
-
-if resposta_input =="1":
-    zona_forward = raw_input("Qual o nome da zona forward?")
-    delete_zone(zone_forward)   
-    os.system("rm /var/named/"+zona_forward+".hosts")
-
-elif resposta_input =="2":
-    dominio_virtual = raw_input("Qual o nome do dominio de VirtualHost que pretende eliminar?")
-    ip_virtual = raw_input("Qual o Ip do VirtualHost que pretende eliminar?")
-    delete_zone(dominio_virtual)
-    os.system("rm /var/named/"+dominio_virtual+".hosts")
-    delete_virtualHost(ip_virtual)
-    os.system("rmdir /"+dominio_virtual)
-
-
-
-elif resposta_input =="3":
-    ip_reverse = raw_input("Qual o ip da zona reverse?")
-
-    delete_zone(ip_reverse)
-    os.system("rm /var/named/"+ip_reverse+".in-addr.arpa.hosts")
-
 def delete_zone(dominio):
     fin = open("teste.txt", "r")
     data = fin.readlines()
@@ -51,3 +28,24 @@ def delete_virtualHost(ip_virtual_host):
     fout =open("/etc/httpd/conf/httpd.conf", "w")
     fout.writelines(data)
     fout.close()
+    
+resposta_input = raw_input("Pretende eliminar o que?(1-zona forward /n 2-virtualhost /n 3-zona reverse):")
+
+if resposta_input =="1":
+    zona_forward = raw_input("Qual o nome da zona forward?")
+    delete_zone(zone_forward)   
+    os.system("rm /var/named/"+zona_forward+".hosts")
+
+elif resposta_input =="2":
+    dominio_virtual = raw_input("Qual o nome do dominio de VirtualHost que pretende eliminar?")
+    ip_virtual = raw_input("Qual o Ip do VirtualHost que pretende eliminar?")
+    delete_zone(dominio_virtual)
+    os.system("rm /var/named/"+dominio_virtual+".hosts")
+    delete_virtualHost(ip_virtual)
+    os.system("rmdir /"+dominio_virtual)
+
+elif resposta_input =="3":
+    ip_reverse = raw_input("Qual o ip da zona reverse?")
+
+    delete_zone(ip_reverse)
+    os.system("rm /var/named/"+ip_reverse+".in-addr.arpa.hosts")
